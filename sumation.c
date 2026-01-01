e54fa009fe4dc28fc7610234a009dee3c42287b4
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2026-01-01 12:17:13 +0330

    updated gitignore and added sumation.c

---
+++ sumation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int a[10] = { 1,2,3,4,5,6,7,8,9,10 };
+    int sum = 0;
+
+    for (int i = 0; i < 10; i++) {
+        sum += a[i];
+    }
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
