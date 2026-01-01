8dfda14fd9f473101a62ebb8f928af4c3e173af9
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2026-01-01 16:18:00 +0330

    updated assembly and alu controller

--- AluController.v
+++ AluController.v
@@ -15,48 +15,38 @@ module AluController (
             aluOpc = aluOp;
         end
         else begin
-            // Type-C decode using func field 
             case (func)
                 9'b000000001: begin
-                    // MoveTo Ri : Ri <- R0
-                    aluOpc = 3'b101;  // Pass In1
+                    aluOpc = 3'b101;  
                     moveTo = 1'b1;
                 end
 
                 9'b000000010: begin
-                    // MoveFrom Ri : R0 <- Ri
 
                     aluOpc = 3'b110;  
                 end
 
                 9'b000000100: begin
-                    // Add Ri : R0 <- R0 + Ri
                     aluOpc = 3'b000;  
                 end
 
                 9'b000001000: begin
-                    // Sub Ri : R0 <- R0 - Ri
-                    aluOpc = 3'b001;  // SUB
+                    aluOpc = 3'b001;  
                 end
 
                 9'b000010000: begin
-                    // And Ri : R0 <- R0 & Ri
-                    aluOpc = 3'b010;  // AND
+                    aluOpc = 3'b010;  
                 end
 
                 9'b000100000: begin
-                    // Or Ri : R0 <- R0 | Ri
-                    aluOpc = 3'b011;  // OR
+                    aluOpc = 3'b011; 
                 end
 
                 9'b001000000: begin
-                    // Not Ri : R0 <- NOT Ri
-
                     aluOpc = 3'b100;  
                 end
 
                 9'b010000000: begin
-                    // Nop : No operation
                     aluOpc = 3'b101; 
                     noOp   = 1'b1;
                 end
--- duc/sumation.asm
+++ duc/sumation.asm
@@ -1,19 +1,10 @@
-; Array:   ram[0x0100 .. 0x0109] = 1..10
-; Result:  ram[0x0110]
-; Program: starts at ram[0x0000]
 
-; ====== Registers ======
-; R0 = accumulator (defined by ISA)
-; R1 = running sum
+ANDI   0x000      
+MOVETO R1         
 
-; sum = 0
-ANDI   0x000        ; R0 = 0
-MOVETO R1           ; R1 = 0
-
-; sum += a[0..9]  (array stored in RAM 0x0100..0x0109)
-LOAD   0x0100       ; R0 = a[0]
-ADD    R1           ; R0 = R0 + R1
-MOVETO R1           ; R1 = sum
+LOAD   0x0100       
+ADD    R1           
+MOVETO R1         
 
 LOAD   0x0101
 ADD    R1
@@ -51,10 +42,8 @@ LOAD   0x0109
 ADD    R1
 MOVETO R1
 
-; store result
-MOVEFROM R1         ; R0 = sum
-STORE  0x0110       ; M[0x0110] = sum
+MOVEFROM R1         
+STORE  0x0110     
 
-; no HALT instruction -> loop forever at END
 END:
-JUMP   END          ; infinite loop
+JUMP   END         
